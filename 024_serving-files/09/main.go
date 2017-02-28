6972c23abfb8ff51b3ab5d3e39a2e9bf8e4156ca
LeeTrent <Lee.Trent.1@gmail.com>
2017-02-28 10:27:13 -0600

    Adds 024_serving-files/09

---
+++ 024_serving-files/09/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+var templ *template.Template
+
+func init() {
+	templ = template.Must(template.ParseFiles("templates/index.gohtml"))
+}
+
+func main() {
+	http.HandleFunc("/", index)
+	http.Handle("/public/",
+		http.StripPrefix("/public", http.FileServer(http.Dir("./public"))))
+	http.ListenAndServe(":8080", nil)
+}
+
+
+func index(res http.ResponseWriter, req *http.Request ) {
+	err := templ.Execute(res, nil)
+	if err != nil {
+		log.Fatalln("Template 'templates/index.gohtml' did not execute", err)
+	}
+}
